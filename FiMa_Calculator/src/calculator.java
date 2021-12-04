c3d89c9d2d3177b3a04aaacd61f47967f2206af9
tomasWag <tomas.wagner1@web.de>
2021-12-04 22:22:14 +0100

    Submit

--- FiMa_Calculator/src/calculator.java
+++ FiMa_Calculator/src/calculator.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class calculator {
     double interest;
     double capital;
     double interestRate;
     double time;
+    String resultType;
 
 
     public void main(String[] args) {
-            System.out.println(getLinearInterest(100, 5,10));
+        System.out.println(getLinearInterest(100, 5,10));
+        resultQuery();
+        multipleInput();
     }
 
   
@@ -19,35 +23,50 @@ public class calculator {
         return interest;
     }
     
-    public double getLinearInterestAtTime(double startCapital, double , double )
+    //public double getLinearInterestAtTime(double startCapital, double , double )
     
     //Zinsrechnung dynamische Zinsrechnung, gesucht Barwert K0
-    public double getDynamicInterest(double )
-    
-
+    public double getDynamicInterest(double endCapital, double time, double q){
+        interest = (endCapital * (1/Math.pow(q, time));
+        return interest;
     }
 
-    private  void multipleInput() {
-		JTextField zahlenMaxField = new JTextField(5);
-		JTextField zahlenZiehungField = new JTextField(5);
+
+    private void multipleInput() {
+		JTextField capitalField = new JTextField(5);
+		JTextField interestRateField = new JTextField(5);
+		JTextField timeField = new JTextField(5);
 
 		JPanel myPanel = new JPanel();
 		myPanel.add(new JLabel("Capital:"));
-		myPanel.add(zahlenMaxField);
+		myPanel.add(capitalField);
 		myPanel.add(Box.createHorizontalStrut(15)); // a spacer
 		myPanel.add(new JLabel("Interest Rate:"));
-		myPanel.add(zahlenZiehungField);
-        myPanel.add(new JLabel("Time (in years)::"));
-        myPanel.add(zahlenZiehungField);
+		myPanel.add(interestRateField);
+        myPanel.add(new JLabel("Time (in years): "));
+        myPanel.add(timeField);
 
 		int result = JOptionPane.showConfirmDialog(null, myPanel,
-				"Please Enter the two Values", JOptionPane.OK_CANCEL_OPTION);
+				"Please Enter three Values", JOptionPane.OK_CANCEL_OPTION);
 		if (result == JOptionPane.OK_OPTION) {
 
-			capital = Integer.parseInt(String.valueOf(zahlenMaxField));
-            interestRate = Integer.parseInt(String.valueOf(zahlenZiehungField));
+			capital = Double.parseDouble(String.valueOf(capitalField));
+            interestRate = Double.parseDouble(String.valueOf(interestRateField));
+            time = Double.parseDouble(String.valueOf(timeField));
 		}
 	}
+
+    public void resultQuery()
+    {
+        String[] options = {"firstOption", "secondOption", "thirdOption"};
+        ImageIcon icon = new ImageIcon("src/images/calculatorImage.png");
+        String n = (String)JOptionPane.showInputDialog(null, "What du you want to calculate?",
+                "Result Query", JOptionPane.QUESTION_MESSAGE, icon, options, options[2]);
+        System.out.println(n);
+        resultType = n;
+    }
+
+
 }
 
 
